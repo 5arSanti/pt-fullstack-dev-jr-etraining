2fdac721056e05de9980cdc9dbf4132351f0f065
5arSanti <santiari05@hotmail.com>
2025-01-11 20:54:56 -0500

    Created Database in SQL

---
+++ database-init/init.sql
@@ -0,0 +1,77 @@
+CREATE DATABASE IF NOT EXISTS PT_FSDJ;
+
+USE PT_FSDJ;
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name VARCHAR(15) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE TABLE modalities (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name VARCHAR(15) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE TABLE inscription_status (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name VARCHAR(15) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    phone VARCHAR(10) NOT NULL,
+    verfied_email_at TIMESTAMP NULL,
+    role_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES roles(id)
+);
+
+CREATE TABLE courses (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    modality_id INT NOT NULL,
+    duration INT NOT NULL,
+    quota INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    FOREIGN KEY (modality_id) REFERENCES modalities(id)
+);
+
+CREATE TABLE user_course (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    user_id INT NOT NULL,
+    course_id INT NOT NULL,
+    inscription_status_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (course_id) REFERENCES courses(id),
+    FOREIGN KEY (inscription_status_id) REFERENCES inscription_status(id)
+);
+
+INSERT INTO roles (name) VALUES
+('Admin'),
+('Coordinador'),
+('Docente'),
+('Estudiante');
+
+INSERT INTO inscription_status (name) VALUES
+('Inscrito'),
+('Aprobado'),
+('Rechazado'),
+('Certificado');
+
+INSERT INTO modalities (name) VALUES
+('Virtual'),
+('Remoto'),
+('Presencial');
